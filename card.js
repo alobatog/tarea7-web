bcd57f4d638f8c78ec4253db507f0dd35cd73f9f
Alejandro Lobato <aalobato@uc.cl>
2020-06-26 15:24:30 -0400

    primeras componentes

---
+++ card.js
@@ -0,0 +1,91 @@
+const cardTemplate = document.createElement('template');
+ 
+cardTemplate.innerHTML = `
+<style>
+    .card {
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    max-width: 300px;
+    margin: auto;
+    text-align: center;
+    font-family: arial;
+    }
+
+    .name {
+    color: black;
+    font-size: 32px;
+    }
+
+    .discount {
+      color: black;
+      font-size: 16px;
+    }
+
+    .discount {
+      color: black;
+      font-size: 16px;
+    }
+
+    .price {
+    color: black;
+    font-size: 22px;
+    }
+
+    .internet {
+    color: blue;
+    font-size: 22px;
+    }
+
+    .normal {
+    color: grey;
+    text-decoration: line-through;
+    font-size: 22px;
+    }
+</style>
+
+<div class="card">
+  <p class="discount"></p>
+  <img class="picture" src="" alt="Denim Jeans" style="width:100%">
+  <h1 class="name">Tailored Jeans</h1>
+  <p class="price"></p>
+  <p class="internet"></p>
+  <p class="normal"></p>
+</div>
+`;
+ 
+class Card extends HTMLElement {
+  constructor() {
+    super();
+ 
+    this._shadowRoot = this.attachShadow({ mode: 'open' });
+    this._shadowRoot.appendChild(cardTemplate.content.cloneNode(true));
+    this.$name = this._shadowRoot.querySelector('.name');
+    this.$normal = this._shadowRoot.querySelector('.normal');
+    this.$internet = this._shadowRoot.querySelector('.internet');
+    this.$price = this._shadowRoot.querySelector('.price');
+    this.$picture = this._shadowRoot.querySelector('.picture');
+    this.$discount = this._shadowRoot.querySelector('.discount');
+
+    //this.$normal.addEventListener('click', () => console.log())
+
+  }
+
+  static get observedAttributes() {
+    return ['normal', 'internet', 'name', 'price', 'picture', 'discount'];
+  }
+ 
+  attributeChangedCallback(name, oldVal, newVal) {
+    this[name] = newVal;
+    this.render();
+  }
+
+  render() {
+    this.$normal.innerHTML = this.normal;
+    this.$internet.innerHTML = this.internet;
+    this.$name.innerHTML = this.name;
+    this.$price.innerHTML = this.price;
+    this.$picture.src = this.picture;
+    this.$discount.innerHTML = ` ${this.discount} de descuento`;
+  }
+}
+ 
+window.customElements.define('my-card', Card);
---
+++ rating.js
@@ -0,0 +1,84 @@
+const ratingTemplate = document.createElement('template');
+
+ratingTemplate.innerHTML = `
+    <style>
+        .rating{
+            padding: 10px;
+        }
+
+        .starRated {
+            cursor: pointer;
+            padding: 5px;
+        }
+
+        .starNotRated{
+            padding: 5px;
+            cursor: pointer;
+            filter: grayscale(100%);
+        }
+    </style>
+
+    <div class='rating'></div>
+`
+class Rating extends HTMLElement {
+    constructor() {
+        super();
+        this._shadowRoot = this.attachShadow({mode: 'open'});
+        this._shadowRoot.appendChild(ratingTemplate.content.cloneNode(true));
+        this.$rating = this._shadowRoot.querySelector('.rating');
+    }
+
+    setStars(n){
+        let stars = this.$rating.getElementsByTagName('img');
+        for(let i = 0; i < this.max; i++){
+            if(i <= +n - 1){ 
+                stars[i].className = 'starRated'; 
+            }
+            else {
+                stars[i].className = 'starNotRated';
+            }
+        }
+    }
+
+    createStars() {
+        for(let i = 1; i <= +this.max; i++){
+            let star = document.createElement('img');
+            star.setAttribute('id', `${i}`);
+            star.setAttribute('class', 'starRated');
+            star.setAttribute('src', this.picture)
+            star.addEventListener('click', () => this.setStars(star.id))
+            this.$rating.appendChild(star);
+        }
+    }
+
+    createRating() {
+        this.picture = this.pictue ? this.picture : 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT-TvAl13LTtRPcOU3y6KN-qv_TiiThMFYvMg&usqp=CAU'
+        if(!this.max || this.max < 1){
+            this.max = 5;
+        }
+        if(!this.rating || this.rating > this.max || this.rating < 0){
+            this.rating = 1;
+        }
+        this.createStars()
+        this.setStars(this.rating)
+    }
+
+    static get observedAttributes() {
+        return ['rating', 'max', 'picture'];
+    }
+
+    attributeChangedCallback(name, oldVal, newVal){
+        this[name] = newVal;
+        this.render();
+    }
+
+    connectedCallback() {
+        this.createRating();
+    }
+
+    render(){
+        this.createRating
+    }
+}
+
+window.customElements.define('my-rating', Rating);
